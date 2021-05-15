2f86aba1e9ca9b7075ec069a6685f1c0cc50a361
Nkoa Christophe <nkoachristophe@gmail.com>
2021-05-15 23:31:43 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lan="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>posty</title>
+    </head>
+    <body class="bg-gray-200">
+        <nav class="p-6 bg-white flex justify-between mb-6">
+            <ul class="flex items-center">
+                <li>
+                    <a class="p-3" href="">Home</a>
+                </li>
+                <li>
+                    <a class="p-3" href="">Dashboard</a>
+                </li>
+                <li>
+                    <a class="p-3" href="">Post</a>
+                </li>
+            </ul>
+            <ul class="flex items-center">
+                <li>
+                    <a class="p-3" href="">Nkoa Christophe</a>
+                </li>
+                <li>
+                    <a class="p-3" href="">Login</a>
+                </li>
+                <li>
+                    <a class="p-3" href=" {{ route('register') }}">Register</a>
+                </li>
+                <li>
+                    <a class="p-3" href="">Logout</a>
+                </li>
+            </ul>
+        </nav> 
+        @yield('content')
+    </body>
+</html>

4ab772ee3384423f66d77b53e92f73551389162f
Nkoa Christophe <nkoachristophe@gmail.com>
2021-05-16 22:02:49 +0100

    middleware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,23 +11,26 @@
         <nav class="p-6 bg-white flex justify-between mb-6">
             <ul class="flex items-center">
                 <li>
-                    <a class="p-3" href="">Home</a>
+                    <a class="p-3" href="{{ route('home') }}">Home</a>
                 </li>
                 <li>
-                    <a class="p-3" href="">Dashboard</a>
+                    <a class="p-3" href="{{ route('dashboard') }}">Dashboard</a>
                 </li>
                 <li>
-                    <a class="p-3" href="">Post</a>
+                    <a class="p-3" href="/posts">Post</a>
                 </li>
             </ul>
             <ul class="flex items-center">
 
                 @auth
                     <li>
-                        <a class="p-3" href="">Nkoa Christophe</a>
+                        <a class="p-3" href="">{{ auth()->user()->name }}</a>
                     </li>
                     <li>
-                        <a class="p-3" href="">Logout</a>
+                        <form action="{{ route('logout') }}" method="post" class="inline">
+                            @csrf
+                            <button type="submit">Logout</button>
+                        </form>
                     </li>
                 @endauth
 
